f8123c0ce966e8e23d381703797b1fdcbd228532
Junhui Shi <jhshi@Junhuis-MacBook-Air.local>
2022-09-28 22:03:20 -0700

    frontend output 3d

--- frontend/make_gradio.py
+++ frontend/make_gradio.py
@@ -21,7 +21,7 @@ def make_output_image(input_image):
     return output_image
 
 
-def main():
+def demo_0():
     # build a basic browser interface to a Python function
     frontend = gr.Interface(
         fn=make_output_image,  # which Python function are we interacting with?
@@ -37,5 +37,24 @@ def main():
     frontend.launch(share=True, server_port=int(GRADIO_SERVER_PORT))
 
 
+def render_3d(user_input):
+
+    teapot = str(Path(__file__).parent / "teapot.obj")
+    pumpkin = str(Path(__file__).parent / "pumpkin.obj")
+
+    return pumpkin
+
+
+def demo_1():
+    frontend = gr.Interface(
+        fn=render_3d,  # which Python function are we interacting with?
+        inputs=gr.components.Image(type="pil", label="Panorama"),
+        outputs=gr.Model3D(clear_color=[0.0, 0.0, 0.0, 0.0], label="3D Layout"),
+        title="3d reconstruction",  # what should we display at the top of the page?
+    )
+
+    frontend.launch(share=True, server_port=int(GRADIO_SERVER_PORT))
+
+
 if __name__ == "__main__":
-    main()
+    demo_1()
