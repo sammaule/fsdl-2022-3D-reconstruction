18c7fba85526f816907f37c50e00fb896e786b46
anaprietonem <ana@cervest.earth>
2022-10-03 22:10:13 +0200

    added some code

--- model/app.py
+++ model/app.py
@@ -1,11 +1,11 @@
-# import json
+import json
 
 # from PIL import ImageStat
 
-# from horizon_net.horizonnet_reconstruction import HorizonNet
-# import horizon_net.util as util
+from horizon_net.horizonnet_reconstruction import HorizonNet
+import horizon_net.util as util
 
-# model = HorizonNet()
+model = HorizonNet()
 
 
 def handler(event, _context):
@@ -17,12 +17,12 @@ def handler(event, _context):
         str: message
     """
     print("INFO loading image")
-    # image = _load_image(event)
-    # if image is None:
-    #        "statusCode": 400,
-    #    return {
-    #        "message": "neither image_url nor image found in event",
-    #    }
+    image = _load_image(event)
+    if image is None:
+        return {
+            "statusCode": 400,
+            "message": "neither image_url nor image found in event",
+        }
     # print("INFO starting inference")
     # print("INFO image loaded")
     # predictions_dict = model.predict(image)
@@ -33,24 +33,24 @@ def handler(event, _context):
     return {"pred": "Hello from AWS Lambda using Python"}
 
 
-# def _load_image(event):
-#    event = _from_string(event)
-#    event = _from_string(event.get("body", event))
-#    image_url = event.get("image_url")
-#    if image_url is not None:
-#        print("INFO url {}".format(image_url))
-#        return util.read_image_pil(image_url, grayscale=False)
-#        image = event.get("image")
-#    else:
-#        if image is not None:
-#            print("INFO reading image from event")
-#            return util.read_b64_image(image, grayscale=False)
-#        else:
-#            return None
+def _load_image(event):
+    event = _from_string(event)
+    event = _from_string(event.get("body", event))
+    image_url = event.get("image_url")
+    if image_url is not None:
+        print("INFO url {}".format(image_url))
+        return util.read_image_pil(image_url, grayscale=False)
+        image = event.get("image")
+    else:
+        if image is not None:
+            print("INFO reading image from event")
+            return util.read_b64_image(image, grayscale=False)
+        else:
+            return None
 
 
-# def _from_string(event):
-#    if isinstance(event, str):
-#        return json.loads(event)
-#    else:
-#        return event
+def _from_string(event):
+    if isinstance(event, str):
+        return json.loads(event)
+    else:
+        return event
