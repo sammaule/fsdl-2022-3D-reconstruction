135f7f2ff3e971d8b4eb9ceba57bc4b7a777a22b
Junhui Shi <jhshi@Junhuis-MacBook-Air.local>
2022-10-04 01:15:31 -0700

    app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,7 @@ Entrypoint for the frontend application.
 Users can provide a panorama image and returns a 3D layout.
 """
 import base64
+from io import BytesIO
 import json
 import logging
 import os
@@ -13,15 +14,14 @@ from typing import Callable, TypeVar
 import cv2
 from dotenv import load_dotenv
 import gradio as gr
-import numpy as np
 from PIL.Image import Image
 import requests
 
 PB = TypeVar("PB", bound="PredictorBackend")
 
-MODEL_URL = os.getenv("LAMBDA_FUNCTION_URL")
-
 load_dotenv()
+
+MODEL_URL = os.getenv("LAMBDA_FUNCTION_URL")
 
 
 def main(model_url: str) -> None:
@@ -43,7 +43,7 @@ class PredictorBackend:
         """Initialize the predictor backend."""
         self.model_url = model_url
 
-    def predict(self: PB, image: np.ndarray) -> tuple:
+    def predict(self: PB, image: Image) -> tuple:
         """Generate images from prediction."""
         logging.info(f"Sending image to backend at {self.model_url}")
         pumpkin = str(Path(__file__).parent / "pumpkin.obj")
@@ -53,9 +53,11 @@ class PredictorBackend:
 
         return (pumpkin, text)
 
-    def send_images_to_aws_lambda(self: PB, image: np.ndarray):
+    def send_images_to_aws_lambda(self: PB, image: Image):
         """Send images (encode to b64) to aws lambda function."""
-        encoded_image = base64.b64encode(image).decode("utf8")
+        _buffer = BytesIO()  # bytes that live in memory
+        image.save(_buffer, format="png")  # but which we write to like a file
+        encoded_image = base64.b64encode(_buffer.getvalue()).decode("utf8")
 
         headers = {"Content-type": "application/json"}
         data = json.dumps({"image": "data:image/png;base64," + encoded_image})
