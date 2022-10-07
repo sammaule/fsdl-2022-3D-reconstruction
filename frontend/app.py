da0c9f81895e69060decde0dc82a7e37433dfc13
sammaule <sam.maule@gmail.com>
2022-10-07 21:24:26 +0100

    Adds examples to frontend

--- frontend/app.py
+++ frontend/app.py
@@ -75,6 +75,7 @@ def make_frontend(fn):
     """Create the frontend for the application."""
     return gr.Interface(
         fn=fn,
+        examples=["frontend/demos/demo1.png", "frontend/demos/demo2.jpg"],
         inputs=gr.components.Image(type="pil", label="Panorama"),
         outputs=[gr.Model3D(clear_color=[0.0, 0.0, 0.0, 0.0], label="3D Layout")],
         title="3D Reconstruction",
