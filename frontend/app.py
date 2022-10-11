d366e0393912fd999b53aa79314f5ea7e49a10b0
anaprietonem <ana@cervest.earth>
2022-10-11 09:02:12 +0200

    frontend multiple images

--- frontend/app.py
+++ frontend/app.py
@@ -8,19 +8,22 @@ import os
 import gradio as gr
 import open3d as o3d
 import requests
+import rootpath
 
-# MODEL_URL = os.getenv("LAMBDA_FUNCTION_URL")
-MODEL_URL = "https://vajhjo3km2a6bxoll4myhchgze0hucec.lambda-url.eu-west-2.on.aws/"
+MODEL_URL = os.getenv("LAMBDA_FUNCTION_URL")
 SERVER_PORT = int(os.getenv("SERVER_PORT", 5001))
+
+
+os.chdir(rootpath.append()[-1])
 
 from horizon_net.layout_viewer import convert_to_3D
 from horizon_net.preprocess import preprocess
+from horizon_net.skybox_grid import create_skybox
 
 
 def main(model_url):
     """Run the frontend."""
-    predictor = PredictorBackend(model_url)
-    frontend = make_frontend(predictor.predict)
+    frontend = make_frontend(model_url)
     frontend.launch(
         share=True, server_name="0.0.0.0", server_port=SERVER_PORT  # noqa: S104
     )
@@ -32,6 +35,40 @@ class PredictorBackend:
     def __init__(self, model_url):
         """Initialize the predictor backend."""
         self.model_url = model_url
+
+    def predict_multiple_images(self, left, top, bottom, right, back, front):
+        """
+        Generate a 3D layout from multiple images.
+
+        Parameters
+        ----------
+        left : PIL image
+            User uploaded left image of skybox grid
+        top : PIL image
+            User uploaded top image of skybox grid
+        bottom : PIL image
+            User uploaded bottom image of skybox grid
+        right : PIL image
+            User uploaded right image of skybox grid
+        back : PIL image
+            User uploaded back image of skybox grid
+        front : PIL image
+            User uploaded front image of skybox grid
+
+        Returns
+        -------
+        _type_
+            HorizonNet model prediction of the 3D layout.
+        """
+        panorama_image = create_skybox([left, top, bottom, right, back, front])
+        # panorama_image.save('tmp/test_panp.png', format="PNG")
+        processed_image = preprocess(panorama_image)
+        logging.info(f"Sending image to backend at {self.model_url}")
+        response = send_images_to_aws_lambda(processed_image, self.model_url)
+
+        mesh = convert_to_3D(processed_image, response)
+        o3d.io.write_triangle_mesh("3D_object.obj", mesh, write_triangle_uvs=True)
+        return "3D_object.obj"
 
     def predict(self, image):
         """
@@ -71,16 +108,52 @@ def send_images_to_aws_lambda(image, model_url):
     return response.json()
 
 
-def make_frontend(fn):
+def make_frontend(model_url):
+    """Generate Frontend Gradio Layout.
+
+    Parameters
+    ----------
+    model_url : str
+        URL pointing to the deployed AWS Lambda function
+
+    Returns
+    -------
+    _type_
+        Gradio Frontend Blocks
+    """
+    predictor = PredictorBackend(model_url)
     """Create the frontend for the application."""
-    return gr.Interface(
-        fn=fn,
-        examples=["frontend/demos/demo1.png", "frontend/demos/demo2.jpg"],
-        inputs=gr.components.Image(type="pil", label="Panorama"),
-        outputs=gr.Model3D(clear_color=[0.0, 0.0, 0.0, 0.0], label="3D Layout"),
-        title="3D Reconstruction",
-        allow_flagging="never",
-    )
+    with gr.Blocks() as demo:
+        gr.Markdown("Flip text or image files using this demo.")
+        with gr.Tab("Predict Panorama"):
+            with gr.Row():
+                image_input = gr.Image(type="pil", label="Panorama")
+                image_output = gr.Model3D(
+                    clear_color=[0.0, 0.0, 0.0, 0.0], label="3D Layout"
+                )
+            image_button = gr.Button("Reconstruct 3D Layout")
+        with gr.Tab("Predict Multiple Images"):
+            with gr.Row():
+                image_0 = gr.components.Image(type="pil", label="Skybox_0")
+                image_1 = gr.components.Image(type="pil", label="Skybox_1")
+                image_2 = gr.components.Image(type="pil", label="Skybox_2")
+                image_3 = gr.components.Image(type="pil", label="Skybox_3")
+                image_4 = gr.components.Image(type="pil", label="Skybox_4")
+                image_5 = gr.components.Image(type="pil", label="Skybox_5")
+                image_output = gr.Model3D(
+                    clear_color=[0.0, 0.0, 0.0, 0.0], label="3D Layout"
+                )
+
+            image_button_multiple = gr.Button("Reconstruct 3D Layout")
+
+        image_button.click(predictor.predict, inputs=image_input, outputs=image_output)
+        image_button_multiple.click(
+            predictor.predict_multiple_images,
+            inputs=[image_0, image_1, image_2, image_3, image_4, image_5],
+            outputs=image_output,
+        )
+
+    return demo
 
 
 if __name__ == "__main__":
